f95139b2f9c0a8e88810d0722d79d367269ddb48
Ann Catherine Jose <ann.jose@gmail.com>
2018-11-20 23:01:59 -0800

    Implemented a simple table view in HomeViewController to demonstrate how to display level 1 and level 2 view controllers through co-ordinators

--- Optima/Optima/Home/HomeViewController.swift
+++ Optima/Optima/Home/HomeViewController.swift
@@ -13,6 +13,8 @@ class HomeViewController: UIViewController {
     var viewModel: HomeViewModel!
     var coordinator: HomeCoordinatorProtocol?
     
+    private var tableView: UITableView!
+    
     init() {
         super.init(nibName: nil, bundle: nil)
         print("HomeViewController:init()")
@@ -27,16 +29,17 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         print("HomeViewController:viewDidLoad()\n")
+        
+        viewModel.configure()
         configureView()
+        configureConstraints()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         print("HomeViewController:viewWillDisappear()")
     }
     
-    func configureView() {
-        
-        viewModel.configure()
+    private func configureView() {
         
         view.backgroundColor = .white
         title = viewModel.title
@@ -44,6 +47,29 @@ class HomeViewController: UIViewController {
         let signOutButton = UIBarButtonItem(title: viewModel.signOutButtonTitle, style: .plain,
                                             target: self, action: #selector(signOut))
         navigationItem.rightBarButtonItem = signOutButton
+        
+        tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+
+        self.view.addSubview(tableView)
+    }
+    
+    private func configureConstraints() {
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        let constraints: [NSLayoutConstraint] = [
+            
+            // Anchor the top-level container view to the main view
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+        ]
+        NSLayoutConstraint.activate(constraints)
+
     }
     
     @objc private func signOut() {
@@ -55,5 +81,26 @@ class HomeViewController: UIViewController {
     
     deinit {
         print("HomeViewController:deinit()")
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row selected \(indexPath)")
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        let itemViewModel = viewModel.itemViewModel(forIndexPath: indexPath)
+        cell.textLabel!.text = itemViewModel.name
+        return cell
     }
 }
--- Optima/Optima/Home/HomeViewModel.swift
+++ Optima/Optima/Home/HomeViewModel.swift
@@ -12,9 +12,12 @@ class HomeViewModel {
     var title = ""
     var signOutButtonTitle = ""
     
+    var items = [ItemViewModel]()
+    
     func configure() {
         title = "Home"
         signOutButtonTitle = "Sign Out"
+        loadItems()
     }
     
     func signOut() -> Bool {
@@ -22,4 +25,20 @@ class HomeViewModel {
         UserDefaults.standard.set(false, forKey: "SIGNED_IN")
         return true
     }
+    
+    func itemViewModel(forIndexPath indexPath: IndexPath) -> ItemViewModel {
+        return items[indexPath.row]
+    }
+    
+    private func loadItems() {
+        items.append(ItemViewModel(name: "Item 1", description: "Details of item 1"))
+        items.append(ItemViewModel(name: "Item 2", description: "Details of item 2"))
+        items.append(ItemViewModel(name: "Item 3", description: "Details of item 3"))
+        items.append(ItemViewModel(name: "Item 4", description: "Details of item 4"))
+    }
+}
+
+struct ItemViewModel {
+    let name: String
+    let description: String
 }
